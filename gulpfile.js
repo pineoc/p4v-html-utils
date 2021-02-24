2058004ab9a639a5e1656aebd27f1a297d98b48b
pineoc <kingsuk92@gmail.com>
2021-02-25 01:04:54 +0900

    gulp deploy addd

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,36 @@
 /// <binding AfterBuild="build" />
 var gulp = require("gulp");
+var ghPages = require('gulp-gh-pages');
 
 var src = {
-    css: [ "src/**/*.css" ],
-    html: [ "src/**/*.html" ],
-    js: [ "src/**/*.js" ],
+  css: ["src/**/*.css"],
+  html: ["src/**/*.html"],
+  js: ["src/**/*.js"],
 };
 
 var dst = {
-    css: "docs/",
-    html: "docs/",
-    js: "docs/",
+  css: "docs/",
+  html: "docs/",
+  js: "docs/",
 };
 
 gulp.task("build:html", function () {
-    return gulp.src(src.html).pipe(gulp.dest(dst.html));
+  return gulp.src(src.html).pipe(gulp.dest(dst.html));
 });
 gulp.task("build:js", function () {
-    return gulp.src(src.js).pipe(gulp.dest(dst.js));
+  return gulp.src(src.js).pipe(gulp.dest(dst.js));
 });
 gulp.task("build:css", function () {
   return gulp.src(src.css).pipe(gulp.dest(dst.css));
 });
 gulp.task("build", gulp.series("build:css", "build:js", "build:html"));
 
+gulp.task("deploy", function () {
+  const option = {
+    branch: "publish",
+  };
+  return gulp.src("docs/**/*").pipe(ghPages(option));
+});
+
 // default
-gulp.task("default",  gulp.series("build"));
+gulp.task("default", gulp.series("build"));
